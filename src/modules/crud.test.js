a17adec04d14ea3be6e6c4f81aa4053ec17f64fb
Alfred <kalumunabisimwa5@gmail.com>
2022-03-31 16:57:16 +0200

    test remove task

--- src/modules/crud.test.js
+++ src/modules/crud.test.js
@@ -1,11 +1,13 @@
-import { addTask, removeTask } from '../__mock__/crud.js';
-
-describe('crud', () => {
-  test('add task', () => {
-    expect(addTask(1, false, 'love life')).toEqual([
-      { index: 1, completed: false, description: 'love life' },
+import { addTask, removeTask } from "../__mock__/crud.js";
+describe("crud", () => {
+  test("add task", () => {
+    expect(addTask(1, false, "love life")).toEqual([
+      { index: 1, completed: false, description: "love life" },
     ]);
   });
-
-
+  test("removeTask", () => {
+    expect(removeTask(1, true, "remove Task")).toEqual([
+      { index: 1, completed: true, description: "remove task" },
+    ]);
+  });
 });
