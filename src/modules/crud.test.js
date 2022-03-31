3255fe882b1d062c5cab42c18c2b9a7f4c988d45
Rizwan-learn <92742850+Rizwan-learn@users.noreply.github.com>
2022-03-31 16:14:49 +0100

    Update crud.test.js

--- src/modules/crud.test.js
+++ src/modules/crud.test.js
@@ -1,13 +1,30 @@
 import { addTask, removeTask } from "../__mock__/crud.js";
 describe("crud", () => {
   test("add task", () => {
-    expect(addTask(1, false, "love life")).toEqual([
-      { index: 1, completed: false, description: "love life" },
+    expect(addTask(1, false, "task1")).toEqual([
+      { index: 1, completed: false, description: "task1" },
     ]);
   });
-  test("removeTask", () => {
-    expect(removeTask(1, true, "remove Task")).toEqual([
-      { index: 1, completed: true, description: "remove task" },
+  
+  test("remove task", () => {
+    addTask(1, false, 'remove task1');
+    addTask(2, false, 'remove task2');
+    addTask(3, false, 'remove task3');
+
+    expect(removeTask(1)).toEqual([
+      {
+        
+        completed: false,
+        description: 'remove task1',
+        index: 1
+      },
+      {
+        completed: false,
+        description: 'remove task2',
+        index: 2
+      }
+
     ]);
+
   });
 });
